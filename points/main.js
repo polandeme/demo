4c677b4d89f31cfbabde16d603f512e142ef9596
polandeme <polandeme@gmail.com>
2014-12-02 23:41:20 +0800

    finished

--- points/main.js
+++ points/main.js
@@ -11,8 +11,7 @@
   var c_point = [];
   var t_pos = [];
   var cp_index = null;
-  var dir = ['+', '-'];
-  for(var i = 0; i < 150; i++) {
+  for(var i = 0; i < 120; i++) {
     var a = {
               x: canvas.width * Math.random(),
               vx: ran().vx,
@@ -29,22 +28,25 @@
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for(var i = 0; i < a_point.length; i++) {
       ctx.beginPath();
-      ctx.fillStyle = '#828d8d';
+      ctx.fillStyle = '#B6B6B6';
+      ctx.strokeStyle = '#eee';
+      ctx.lineWidth = 10;
       if(i === cp_index) {
-      ctx.strokeStyle = 'red';
-      ctx.fillStyle = '#828d8d';
-      ctx.fillText('hello', a_point[cp_index].x + 5, a_point[cp_index].y -5 );
-      ctx.lineWidth = 3;
-      ctx.arc(a_point[cp_index].x, a_point[cp_index].y ,5, 0, 2*Math.PI);
-      ctx.fill();
-      ctx.stroke();
-      ctx.closePath();
+        ctx.fillStyle = '#B6B6B6'; //#828d8d
+        ctx.strokeStyle = '#eee';
+        ctx.lineWidth = 12;
+        ctx.arc(a_point[cp_index].x, a_point[cp_index].y ,3, 0, 2*Math.PI);
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
+        w_words(a_point[cp_index].x, a_point[cp_index].y);
       } else {
         var x = a_point[i].x;
         var y = a_point[i].y;
         c_point[i].x = x;
         c_point[i].y = y;
-        ctx.arc(x, y ,2, 0, 2*Math.PI);
+        ctx.arc(x, y ,3, 0, 2*Math.PI);
+        ctx.stroke();
         ctx.fill();
         ctx.closePath();        
       }
@@ -56,20 +58,20 @@
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for(var i = 0; i < a_point.length; i++) {
       ctx.beginPath();
-      ctx.fillStyle = 'red';
+      ctx.fillStyle = '#B6B6B6';
       var x = c_point[i].x;
       var y = c_point[i].y;
-      ctx.arc(x, y ,2, 0, 2*Math.PI);
+      ctx.arc(x, y ,13, 0, 2*Math.PI);
       ctx.fill();
 
       if(p && ctx.isPointInPath(p.x, p.y)) {
           cp_index = i;
-          ctx.fillStyle = 'blue';   
+         /* ctx.fillStyle = 'blue';   
           ctx.beginPath(); 
           ctx.arc(c_point[cp_index].x, c_point[cp_index].y ,15, 0, 2*Math.PI);
-          ctx.fill();  
+          ctx.fill();  */
           t_pos[0] = p;
-      }else if( p &&t_pos[0] && ((p.x - t_pos[0].x) > 4  || (p.x - t_pos[0].x) < -2 ) ){
+      }else if( p &&t_pos[0] && ((p.x - t_pos[0].x) > 6  || (p.x - t_pos[0].x) < -6)  ){
         t_pos[0] = p;
         window.cancelAnimationFrame(reId); 
         reId = requestAnimationFrame(moveTo);
@@ -81,12 +83,12 @@
 
   function connect() {
 
-    for(var i = 0; i < 70; i++) {
-      for(var j = 0; j< 50; j++) {
+    for(var i = 0; i < 100; i++) {
+      for(var j = 0; j< 100; j++) {
         if((a_point[i].x - a_point[j].x < 100) && (a_point[i].y - a_point[j].y < 100) && (a_point[i].y - a_point[j].y > -100) && (a_point[i].x - a_point[j].x > -100)) {
           ctx.beginPath();
           ctx.strokeStyle = 'gray';
-          ctx.lineWidth = .5;
+          ctx.lineWidth = .2;
           ctx.moveTo(a_point[i].x, a_point[i].y);
           ctx.lineTo(a_point[j].x, a_point[j].y);
           ctx.stroke();
@@ -96,19 +98,11 @@
     }
   }
 
-function reCurPoint(ts) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  console.log('reCurPoint');
-  ctx.fillStyle = 'gray';   
-  ctx.beginPath(); 
-  ctx.arc(c_point[ts].x, c_point[ts].y ,15, 0, 2*Math.PI);
-  ctx.fill();  
-}
   function moveTo() {
     // ctx.clearRect(0, 0, canvas.width, canvas.height);
     for(var i = 0; i < a_point.length; i++) {
       if(i === cp_index) {
-        console.log(cp_index);
+        // console.log(cp_index);
         a_point[cp_index].x = a_point[cp_index].x;
         a_point[cp_index].y = a_point[cp_index].y;
       }else {
@@ -117,7 +111,6 @@ function reCurPoint(ts) {
         a_point[i].y +=  a_point[i].vy;        
       }
     }
-    // reDraw();
     draw();
     check();
     connect();
@@ -137,13 +130,14 @@ function reCurPoint(ts) {
 
   function check() {
     for(var i = 0; i < c_point.length; i++) {
-      if(c_point[i].x > window.innerWidth || c_point[i].y > window.innerHeight)
+
+      if(c_point[i].x > window.innerWidth || c_point[i].x < 0  )
       {
-        c_point[i].x -= 1000 * Math.random()  + ran().vx;
-        c_point[i].y = 1000 * Math.random() + ran().vx;
-      } else if(c_point[i].x <0 || c_point[i].y < 0) {
-        c_point[i].x = 1000 * Math.random() + ran().vx;
-        c_point[i].y = 1000 * Math.random() + ran().vx;
+        a_point[i].vx = -a_point[i].vx;
+        a_point[i].vy = a_point[i].vy;  
+      } else if(c_point[i].y > canvas.height || c_point[i].y < 0) {
+        a_point[i].vx = a_point[i].vx;
+        a_point[i].vy = -a_point[i].vy;  
       }
     }
     
@@ -154,19 +148,18 @@ function reCurPoint(ts) {
     mousemove: function(e) {
       var p = getEventPosition(e);
         var ts = reDraw(p);
-        console.log(ts);
         connect();
       },
       mousemleave: function(e) {
-        console.log('out');
+        // console.log('out');
       var p = getEventPosition(e);
         var ts = reDraw(p);
-        console.log(ts);
         connect();
       }
   })
 
   function getEventPosition(e) {
+    var e = e || window.event;
     var x, y;
     if(e.layerX || e.layerX == 0) {
       x = e.layerX;
@@ -180,7 +173,30 @@ function reCurPoint(ts) {
       y : y,
     }
   }
-  draw();
+
+  // write words 
+  function w_words(x, y, data) {
+    var lData = localStorage.getItem('data').split(':,');
+    var data = data ? data.data : lData;
+    var i = Math.round(Math.random() * data.length);
+    ctx.fillStyle = 'blue';
+    ctx.fillText(data[cp_index % data.length], x + 5, y - 5);
+  }
+
+draw();
 requestAnimationFrame(moveTo); 
 
+$.ajax({
+  async:false,
+  url: 'data.json',
+  type: 'get',
+  dataType: 'json',
+  success: function(data) {
+    console.log(data);
+    window.localStorage.setItem('data', data.data)
+  }, 
+  error: function(error) {
+    console.log(error);
+  }
+});
 })();
